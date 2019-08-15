edfe94198e54e8b28bf1cbea1da5f829cb564a51
trips2821 <trips2821@gmail.com>
2019-08-14 19:34:58 -0600

    card_blog app

--- card_blog/admin.py
+++ card_blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from card_blog.models import Card
+
+admin.site.register(Card)
--- card_blog/models.py
+++ card_blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
+
+    created_dt = models.DateTimeField(auto_now=True)
